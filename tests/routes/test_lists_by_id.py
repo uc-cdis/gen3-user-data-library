ce82c4c97cdb5a0d4d388ed76286d17f021c0ee1
Albert Snow <ajsnow2012@gmail.com>
2024-10-04 20:49:47 -0500

    fixing test that works now for some reason

--- tests/routes/test_lists_by_id.py
+++ tests/routes/test_lists_by_id.py
@@ -87,73 +87,12 @@ class TestUserListsRouter(BaseTestRouter):
         note: getting weird test behavior if I try to use valid lists, so keeping local until that is resolved
         """
         headers = {"Authorization": "Bearer ofa.valid.token"}
-        special_list_a = {
-            "name": "My Saved List 1",
-            "items": {
-                "drs://dg.4503:943200c3-271d-4a04-a2b6-040272239a64": {
-                    "dataset_guid": "phs000001.v1.p1.c1",
-                    "type": "GA4GH_DRS",
-                },
-                "CF_1": {
-                    "name": "Cohort Filter 1",
-                    "type": "Gen3GraphQL",
-                    "schema_version": "c246d0f",
-                    "data": {
-                        "query": "query ($filter: JSON) { _aggregation { subject (filter: $filter) "
-                                 "{ file_count { histogram { sum } } } } }",
-                        "variables": {
-                            "filter": {
-                                "AND": [
-                                    {"IN": {"annotated_sex": ["male"]}},
-                                    {"IN": {"data_type": ["Aligned Reads"]}},
-                                    {"IN": {"data_format": ["CRAM"]}},
-                                ]
-                            }
-                        },
-                    },
-                }}}
-        special_list_b = {
-            "name": "õ(*&!@#)(*$%)() 2",
-            "items": {
-                "CF_1": {
-                    "name": "Some cohort I made with special characters: !@&*(#)%$(*&.?:<>õ",
-                    "type": "Gen3GraphQL",
-                    "schema_version": "aacc222",
-                    "data": {
-                        "query": "query ($filter: JSON,) {\n"
-                                 "    subject (accessibility: accessible, offset: 0, first: 20, , filter: $filter,) {\n"
-                                 "      \n    project_id\n    \n\n    data_format\n    \n\n    race\n    \n\n"
-                                 "    annotated_sex\n    \n\n    ethnicity\n    \n\n    hdl\n    \n\n    ldl\n    \n    }\n"
-                                 "    _aggregation {\n      subject (filter: $filter, accessibility: accessible) {\n"
-                                 "        _totalCount\n      }\n    }\n  }",
-                        "variables": {
-                            "filter": {
-                                "AND": [
-                                    {"IN": {"project_id": ["tutorial-synthetic_data_set_1"]}},
-                                    {"IN": {"data_type": ["Aligned Reads"]}},
-                                ]
-                            }
-                        },
-                    },
-                },
-                "drs://dg.4503:943200c3-271d-4a04-a2b6-040272239a64": {
-                    "dataset_guid": "phs000001.v1.p1.c1",
-                    "type": "GA4GH_DRS",
-                },
-                "drs://dg.TEST:3418077e-0779-4715-8195-7b60565172f5": {
-                    "dataset_guid": "phs000002.v2.p2.c2",
-                    "type": "GA4GH_DRS",
-                },
-                "drs://dg.4503:edbb0398-fcff-4c92-b908-9e650e0a6eb5": {
-                    "dataset_guid": "phs000002.v2.p2.c1",
-                    "type": "GA4GH_DRS",
-                },
-            },
-        }
-        create_outcomes = [await create_basic_list(arborist, get_token_claims, client, user_list, headers)
-                           for user_list in [special_list_a, special_list_b]]
+
+        await create_basic_list(arborist, get_token_claims, client, VALID_LIST_A, headers)
+        await create_basic_list(arborist, get_token_claims, client, VALID_LIST_B, headers)
+
         body = {
-            "drs://dg.4503:943200c3-271d-4a04-a2b6-040272239a65": {
+            "drs://dg.4503:943200c3-271d-4a04-a2b6-040272239a99": {
                 "dataset_guid": "phs000001.v1.p1.c1",
                 "type": "GA4GH_DRS"
             },
@@ -173,14 +112,14 @@ class TestUserListsRouter(BaseTestRouter):
         response_one = await client.patch("/lists/1", headers=headers, json=body)
         response_two = await client.patch("/lists/2", headers=headers, json=body)
         for response in [response_one, response_two]:
-            updated_list = response.json().get("updated_list", None)
+            updated_list = response.json().get("data", None)
             items = updated_list.get("items", None)
             assert response.status_code == 200
             assert items is not None
             assert items.get("CF_1", None) is not None
             assert items.get("CF_2", None) is not None
             assert items.get('drs://dg.4503:943200c3-271d-4a04-a2b6-040272239a64', None) is not None
-            assert items.get('drs://dg.4503:943200c3-271d-4a04-a2b6-040272239a65', None) is not None
+            assert items.get('drs://dg.4503:943200c3-271d-4a04-a2b6-040272239a99', None) is not None
             if updated_list.get("name", None) == 'õ(*&!@#)(*$%)() 2':
                 assert len(items) == 6
 
