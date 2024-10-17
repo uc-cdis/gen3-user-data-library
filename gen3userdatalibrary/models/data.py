8759be2ae9a8018caae655d41f8e638e0dd43efc
Albert Snow <ajsnow2012@gmail.com>
2024-10-17 16:37:08 -0500

    changing data structure

--- gen3userdatalibrary/models/data.py
+++ gen3userdatalibrary/models/data.py
@@ -5,60 +5,58 @@ WHITELIST = {"items", "name"}
 
 uuid4_regex_pattern = "([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})"
 
-recognized_endpoints_as_regex = {
-    r"^/docs/?$",
-    r"^/redoc/?$",
-    r"^/_version/?$",
-    r"^/_status/?$",
-    r"^/?$",
-    r"^/lists/?$",
-    rf"^/lists/{uuid4_regex_pattern}/?$"}
+recognized_endpoint_functions = {
+    "redirect_to_docs",
+    "get_version",
+    "get_status",
+    "read_all_lists",
+    "upsert_user_lists",
+    "delete_all_lists",
+    "get_list_by_id",
+    "update_list_by_id",
+    "append_items_to_list",
+    "delete_list_by_id"}
 
 endpoints_to_context = {
-    r"^/docs/?$": {"GET": {"resource": "/gen3_data_library/service_info/docs",
-                           "method": "read"}},
-    r"^/redoc/?$": {"GET": {"resource": "/gen3_data_library/service_info/docs",
-                            "method": "read"}},
-    r"^/_version/?$": {"GET": {"resource": "/gen3_data_library/service_info/version",
-                               "method": "read"}},
-    r"^/_status/?$": {"GET": {"resource": "/gen3_data_library/service_info/status",
-                              "method": "read"}},
-    r"^/?$": {"GET": {"resource": "/gen3_data_library/service_info/redoc",
-                      "method": "read"}},
-    r"^/lists/?$": {
-        "GET": {
-            "type": "all",
-            "resource": lambda user_id: get_lists_endpoint(user_id),
-            "method": "read",
-        },
-        "PUT": {
-            "type": "all",
-            "resource": lambda user_id: get_lists_endpoint(user_id),
-            "method": "update",
-            "items": lambda b: list(map(lambda item_to_update: item_to_update["items"], b["lists"]))
-        },
-        "DELETE": {
-            "type": "all",
-            "resource": lambda user_id: get_lists_endpoint(user_id),
-            "method": "delete"}},
-    rf"^/lists/{uuid4_regex_pattern}/?$": {
-        "GET": {
-            "type": "id",
-            "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
-            "method": "read"},
-        "PUT": {
-            "type": "id",
-            "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
-            "method": "update",
-            "items": lambda b: b["items"]
-        },
-        "PATCH": {
-            "type": "id",
-            "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
-            "method": "update",
-            "items": identity
-        },
-        "DELETE": {
-            "type": "id",
-            "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
-            "method": "delete"}}}
+    "redirect_to_docs": {"resource": "/gen3_data_library/service_info/redoc",
+                         "method": "read"},
+    "get_version": {"resource": "/gen3_data_library/service_info/version",
+                    "method": "read"},
+    "get_status": {"resource": "/gen3_data_library/service_info/status",
+                   "method": "read"},
+    "read_all_lists": {
+        "type": "all",
+        "resource": lambda user_id: get_lists_endpoint(user_id),
+        "method": "read",
+    },
+    "upsert_user_lists": {
+        "type": "all",
+        "resource": lambda user_id: get_lists_endpoint(user_id),
+        "method": "update",
+        "items": lambda b: list(map(lambda item_to_update: item_to_update["items"], b["lists"]))
+    },
+    "delete_all_lists": {
+        "type": "all",
+        "resource": lambda user_id: get_lists_endpoint(user_id),
+        "method": "delete"},
+    "get_list_by_id": {
+        "type": "id",
+        "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
+        "method": "read"},
+    "update_list_by_id": {
+        "type": "id",
+        "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
+        "method": "update",
+        "items": lambda b: b["items"]
+    },
+    "append_items_to_list": {
+        "type": "id",
+        "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
+        "method": "update",
+        "items": identity
+    },
+    "delete_list_by_id": {
+        "type": "id",
+        "resource": lambda user_id, list_id: get_list_by_id_endpoint(user_id, list_id),
+        "method": "delete"}
+}
