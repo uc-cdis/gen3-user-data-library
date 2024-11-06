0703cd5a5252ae6ae364b7691ad320e917d4e1ea
Albert Snow <ajsnow2012@gmail.com>
2024-11-06 14:54:40 -0600

    fix sh files

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Current Directory: ${CURRENT_DIR}"
 
 export ENV="test"
-source "${CURRENT_DIR}/../tests/.env"
-source "${CURRENT_DIR}/_common_setup.sh"
+source "${CURRENT_DIR}/tests/.env"
+source "${CURRENT_DIR}/bin/_common_setup.sh"
 
 echo "running tests w/ 'pytest'..."
 poetry run pytest -vv --cov-config=.coveragerc --cov=gen3userdatalibrary --cov-report term-missing:skip-covered --cov-fail-under 90 --cov-report html:_coverage --cov-branch
