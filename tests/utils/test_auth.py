94a3c7ebdb728414785017c60d1a35460981e6ce
Albert Snow <ajsnow2012@gmail.com>
2024-11-11 15:27:49 -0600

    mock id works

--- tests/utils/test_auth.py
+++ tests/utils/test_auth.py
@@ -6,10 +6,53 @@ from starlette.datastructures import Headers
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
 
-from gen3userdatalibrary import config
-from gen3userdatalibrary.auth import _get_token, authorize_request
+from gen3userdatalibrary import config, auth
+from gen3userdatalibrary.auth import (
+    _get_token,
+    authorize_request,
+)
 from gen3userdatalibrary.main import route_aggregator
 from tests.routes.conftest import BaseTestRouter
+
+
+@pytest.fixture
+def mock_get_user_id(mocker):
+    mock_user_id = mocker.patch(
+        "gen3userdatalibrary.auth.get_user_id", new_callable=AsyncMock
+    )
+    mock_user_id.return_value = "mock_user_id"
+    return mock_user_id
+
+
+@pytest.fixture
+def mock_alt_get_token(mocker):
+    mock_token = mocker.patch(
+        "gen3userdatalibrary.auth._alt_get_token", new_callable=AsyncMock
+    )
+    mock_token.return_value = "alt mock"
+    return mock_token
+
+
+@pytest.fixture
+def mock_get_token(mocker):
+    mock_token = mocker.patch(
+        "gen3userdatalibrary.auth._get_token", new_callable=AsyncMock
+    )
+    mock_token.return_value = "normal mock"
+    return mock_token
+
+
+@pytest.fixture
+def mock_both_token(mocker):
+    mock_token = mocker.patch(
+        "gen3userdatalibrary.auth._get_token", new_callable=AsyncMock
+    )
+    mock_token.return_value = "normal mock"
+    alt_mock_token = mocker.patch(
+        "gen3userdatalibrary.auth._alt_get_token", new_callable=AsyncMock
+    )
+    alt_mock_token.return_value = "alt mock"
+    return mock_token, alt_mock_token
 
 
 @pytest.mark.asyncio
@@ -57,7 +100,7 @@ class TestAuthRouter(BaseTestRouter):
             else:
                 assert output == token_param
 
-    async def test_authorize_request(self):
+    async def test_authorize_request(self, monkeypatch):
         example_creds = HTTPAuthorizationCredentials(
             scheme="Bearer", credentials="my_access_token"
         )
@@ -78,4 +121,34 @@ class TestAuthRouter(BaseTestRouter):
                 None,  # example_creds,
                 example_request,
             )
-        assert NotImplemented
+
+    async def test_id(
+        self,
+        mocker,
+    ):
+        mock_token = mocker.patch(
+            "gen3userdatalibrary.auth._get_token", new_callable=AsyncMock
+        )
+        mock_token.return_value = "normal mock"
+        alt_mock_token = mocker.patch(
+            "gen3userdatalibrary.auth.get_user_id", new_callable=AsyncMock
+        )
+        alt_mock_token.return_value = "mock id"
+        example_request = Request(
+            {
+                "type": "http",
+                "method": "GET",
+                "path": "/example",
+                "headers": Headers({"host": "127.0.0.1:8000"}).raw,
+                "query_string": b"name=example",
+                "client": ("127.0.0.1", 8000),
+            }
+        )
+        outcome = await auth.authorize_request(
+            "access",
+            ["/users/1/user-data-library/lists"],
+            None,  # example_creds,
+            example_request,
+        )
+
+        assert outcome is False
