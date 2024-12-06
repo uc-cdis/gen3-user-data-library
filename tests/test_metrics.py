55bfcaad3531f46dbd2756599ee566dd35763af9
Albert Snow <ajsnow2012@gmail.com>
2024-12-06 14:25:15 -0600

    remove unused metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,5 @@
 import pytest
 
-from gen3userdatalibrary.metrics import Metrics
 from gen3userdatalibrary.routes import route_aggregator
 from tests.routes.conftest import BaseTestRouter
 
@@ -8,17 +7,3 @@ from tests.routes.conftest import BaseTestRouter
 @pytest.mark.asyncio
 class TestConfigRouter(BaseTestRouter):
     router = route_aggregator
-
-    async def test_add_user_list_counter(self):
-        metrics_disabled = Metrics("/foo", False)
-        metrics_disabled.add_user_list_counter()
-        metrics_enabled = Metrics("/bar", True)
-        with pytest.raises(ValueError):
-            metrics_enabled.add_user_list_counter()
-
-    async def test_add_user_list_item_counter(self):
-        metrics_disabled = Metrics("/foo", False)
-        metrics_disabled.add_user_list_item_counter()
-        metrics_enabled = Metrics("/bar", True)
-        with pytest.raises(ValueError):
-            metrics_enabled.add_user_list_item_counter()
