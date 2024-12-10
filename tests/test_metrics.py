ef17ea41abf4a9580011559fa67e8c06689166f8
Albert Snow <ajsnow2012@gmail.com>
2024-12-10 10:11:39 -0600

    adding sanity check tests for metric

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,6 @@
 import pytest
 
+from gen3userdatalibrary.metrics import Metrics
 from gen3userdatalibrary.routes import route_aggregator
 from tests.routes.conftest import BaseTestRouter
 
@@ -7,3 +8,21 @@ from tests.routes.conftest import BaseTestRouter
 @pytest.mark.asyncio
 class TestConfigRouter(BaseTestRouter):
     router = route_aggregator
+
+
+def test_handle_user_lists_gauge():
+    metrics = Metrics("/var/tmp/prometheus_metrics", True)
+    metrics.handle_user_lists_gauge(1, action="CREATE")
+    metrics.handle_user_lists_gauge(1, action="DELETE")
+
+
+def test_handle_user_items_gauge():
+    metrics = Metrics("/var/tmp/prometheus_metrics", True)
+    metrics.handle_user_items_gauge(1, action="CREATE")
+    metrics.handle_user_items_gauge(1, action="DELETE")
+
+
+def test_add_user_list_api_interaction():
+    metrics = Metrics("/var/tmp/prometheus_metrics", True)
+    metrics.add_user_list_api_interaction(name="CREATE")
+    metrics.add_user_list_api_interaction(name="DELETE")
